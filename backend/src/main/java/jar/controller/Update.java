e2dcafbf2dbcbe67bc01619c7a53b2f252e82bd5
krixPixel0o <aniruddh.bellur.03@gmail.com>
2026-08-05 13:37:32 +0530

    Commit 2

--- backend/src/main/java/jar/controller/Update.java
+++ backend/src/main/java/jar/controller/Update.java
@@ -5,7 +5,11 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import jar.model.Student;
 import jar.repo.StudentRepo;
--- backend/src/main/java/jar/model/Student.java
+++ backend/src/main/java/jar/model/Student.java
@@ -1,7 +1,8 @@
 package jar.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.*;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -13,7 +14,7 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
     private String name;
-    @Column(unique=true)
+    @Column(nullable = false, unique=true)
     private String email;
     private String ip;
 
